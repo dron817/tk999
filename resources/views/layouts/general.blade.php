96f9bdd7402bb1b874181f057c3c319122bb21a1
dron817 <myroyalfamily@ya.ru>
2022-02-15 23:47:38 +0500

    прелоадер

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -12,6 +12,7 @@
     <!-- CSS Styles  -->
     <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/main.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/preloader.css') }}"/>
 @yield('custom-css')
 
     <!-- JS Includes before  -->
@@ -44,7 +45,30 @@
 </nav>
     @yield('content')
 
+
+<div class="preloader">
+    <svg>
+        <g>
+            <path d="M 50,100 A 1,1 0 0 1 50,0"/>
+        </g>
+        <g>
+            <path d="M 50,75 A 1,1 0 0 0 50,-25"/>
+        </g>
+        <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                <stop offset="0%" style="stop-color:#BFE2FF;stop-opacity:1" />
+                <stop offset="100%" style="stop-color:#337AB7;stop-opacity:1" />
+            </linearGradient>
+        </defs>
+    </svg>
+</div>
+
 <!-- JS Includes after  -->
+<script>
+    window.onload = function() {
+        document.querySelector('.preloader').classList.add("preloader-remove");
+    };
+</script>
 <script src="{{ asset('assets/js/jquery.json.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 @yield('custom-js-after')
